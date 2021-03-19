8bab368872f2a1f6e934a87d68b8fb6436d36012
Eduardo_Gutierrez <eduardogutierrez318@aragon.unam.mx>
2021-03-19 17:19:44 -0600

    Cuestionario 11. Actividad en clase 19-mar-21

---
+++ src/ico/fes/Automovil.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ico.fes;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author Eduardo Gutierrez
+ */
+public class Automovil {
+    private String marca;
+    private String subMarca;
+    private int modelo;
+    private Color color;
+
+    public Automovil() {
+    }
+
+    public Automovil(String marca, String subMarca, int modelo, Color color) {
+        this.marca = marca;
+        this.subMarca = subMarca;
+        this.modelo = modelo;
+        this.color = color;
+    }
+
+    public Color getColor() {
+        System.out.println("Metiendo el auto al taller de pintura...");
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getSubMarca() {
+        return subMarca;
+    }
+
+    public void setSubMarca(String subMarca) {
+        this.subMarca = subMarca;
+    }
+
+    public int getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(int modelo) {
+        this.modelo = modelo;
+    }
+
+    @Override
+    public String toString() {
+        return "Automovil{" + "marca=" + marca + ", subMarca=" + subMarca + ", modelo=" + modelo + ", color=" + color + '}';
+    }
+
+    public void setModelo(String nsx) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+}
