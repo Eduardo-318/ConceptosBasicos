f897df595a0e3d3a6596a5ac718d654351665c3c
Eduardo_Gutierrez <eduardogutierrez318@aragon.unam.mx>
2021-03-19 17:23:54 -0600

    Tarea 4.

--- src/conceptosbasicos/Conceptosbasicos.java
+++ src/conceptosbasicos/Conceptosbasicos.java
@@ -26,7 +26,20 @@ public class Conceptosbasicos {
         miBocho.setColor(Color.blue);
         System.out.println(miBocho);
 
-       
+     //TAREA Programar los objetos miAcura y miMustang
+        Automovil miAcura = new Automovil();
+        miAcura.setMarca("Acura");
+        miAcura.setSubMarca("NSX");
+        miAcura.setModelo(2013);
+        miAcura.setColor(Color.gray);
+        System.out.println(miAcura);
+
+        Automovil miMustang = new Automovil();
+        miMustang.setMarca("Ford");
+        miMustang.setSubMarca("Mustang");
+        miMustang.setModelo(2010);
+        miMustang.setColor(Color.yellow);
+        System.out.println(miMustang); 
     }
 
 }
